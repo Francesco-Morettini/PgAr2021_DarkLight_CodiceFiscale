ebe2b31a2800437da97206cecf065672463102d3
Matteo <m.mora018@studenti.unibs.it>
2021-04-27 16:26:04 +0200

    Ho aggiunto il file Lezione_4.pdf e creato la classe "Persona"

--- Codice_Fiscale/src/Main.java
+++ Codice_Fiscale/src/Main.java
@@ -1,12 +1,4 @@
 public class Main {
      public static void main(String[] args) {
-         // teniamo questo per le prove
-        System.out.println("ciao");
-         System.out.println("githubbo");
-
-         System.out.println("francesco massello");
-         System.out.println("dobbiamo lavorare su una classe alla volta ognuno, se no si creano conflitti");
-         System.out.println("sono d'accordo");
-
      }
 }
