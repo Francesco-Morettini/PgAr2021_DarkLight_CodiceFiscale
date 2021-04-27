5e5ab799986c30d8c140149713014f0cc5ebd387
desikalakar786 <81826196+desikalakar786@users.noreply.github.com>
2021-04-27 16:52:12 +0200

    KAMAL

--- Codice_Fiscale/src/Persona.java
+++ Codice_Fiscale/src/Persona.java
@@ -1,7 +1,15 @@
+import java.util.Date;
+
 public class Persona {
     private String nome;
-    public Persona(String nome)
-    {
-        this.nome=nome;
+    private String cognome;
+    private String comune_n;
+    private String data_nascita;
+
+    public Persona(String nome, String cognome, String comune_n, String data_nascita) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.comune_n = comune_n;
+        this.data_nascita = data_nascita;
     }
 }
