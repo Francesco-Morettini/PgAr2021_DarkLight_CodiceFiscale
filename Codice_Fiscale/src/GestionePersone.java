8a2e42591b165b897bcf8f968c3f54258d396466
desikalakar786 <81826196+desikalakar786@users.noreply.github.com>
2021-04-28 20:17:28 +0200

    KAMAL

---
+++ Codice_Fiscale/src/GestionePersone.java
@@ -0,0 +1,12 @@
+    import java.util.ArrayList;
+public class GestionePersone {
+
+    private ArrayList <Persona> persone  ;
+
+    public GestionePersone() {
+
+    }
+    public void AddPersona(Persona x) {
+
+    }
+}
--- Codice_Fiscale/src/Persona.java
+++ Codice_Fiscale/src/Persona.java
@@ -4,6 +4,39 @@ public class Persona {
     private String nome;
     private String cognome;
     private String comune_n;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getComune_n() {
+        return comune_n;
+    }
+
+    public void setComune_n(String comune_n) {
+        this.comune_n = comune_n;
+    }
+
+    public String getData_nascita() {
+        return data_nascita;
+    }
+
+    public void setData_nascita(String data_nascita) {
+        this.data_nascita = data_nascita;
+    }
+
     private String data_nascita;
 
     public Persona(String nome, String cognome, String comune_n, String data_nascita) {
