d8859f42ee0d3242cb1d3289f920bbfaac764805
Francesco-Morettini <frcmorettini.01@gmail.com>
2021-04-29 19:05:12 +0200

    Terminato metodo importCodici in classe GestionePersone.

--- Codice_Fiscale/src/Main.java
+++ Codice_Fiscale/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
      public static void main(String[] args){
           GestionePersone start = new GestionePersone();
           start.importPersone();
-          //start.importCodici();
+          start.importCodici();
           //Persona c= new Persona(0,"francesco","morettini",'m',"gavardo","31/12/00");
           //System.out.println(c.cognomeCodice());
      }
